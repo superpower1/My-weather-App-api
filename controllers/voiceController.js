30487c8938f3dac6c8980dcd89d0cc7fc0280d99
Haofu <haofu.zhu@gmail.com>
2018-03-08 23:20:02 +1100

    add melbourne's weather report to voice message

--- controllers/voiceController.js
+++ controllers/voiceController.js
@@ -1,11 +1,23 @@
 const VoiceRes = require('twilio').twiml.VoiceResponse;
+const rp = require('request-promise');
+const owmAPI = process.env.OPEN_WEATHER_MAP_KEY;
+const dataProcessor = require('../lib/dataProcessor');
+
+const apiController = require('./apiController');
 
 const voiceResponse = (req, res) => {
   const twiml = new VoiceRes();
-  twiml.say('Hello, this is alice');
+  const location = "melbourne";
+  rp(`http://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=${owmAPI}`)
+  .then(rawForcast=>{
+    const forecast = dataProcessor.processForecastData(JSON.parse(rawForcast));
+    const tomorrowWeather = forecast[0].description;
+    const tomorrowTemp = forecast[0].temp;
+    twiml.say(`Hello, tomorrow's weather will be ${tomorrowWeather}, temprature is ${tomorrowTemp} celsius degree.`);
 
-  res.type('text/xml');
-  res.send(twiml.toString());
+    res.type('text/xml');
+    res.send(twiml.toString());
+  })
 }
 
 module.exports = {
